0bedf02392f5003cc04f14876a4544c5b4dbfd7c
marcos <estudogit@outlook.com>
2023-03-14 13:21:46 -0300

    testando widgets

--- lib/app/ui/home/home_page.dart
+++ lib/app/ui/home/home_page.dart
@@ -8,19 +8,54 @@ class HomePage extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text("🚧   Work in progress...   🚧",
+      backgroundColor: Colors.grey,
+      body: Center(
+        child: Container(
+          width: 500.0,
+          color: const Color(0xEEE4000F),
+          child: Stack(children: [
+            Text(
+              "Text 1",
               style: TextStyle(fontSize: 24, color: Colors.white),
+            ),
+            Center(
+              child: Container(
+                color: Colors.blue,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        "🚧   Work in progress...   🚧",
+                        style: TextStyle(fontSize: 24, color: Colors.white),
+                      ),
+                      Text(
+                        "🚧   Work in progress...   🚧",
+                        style: TextStyle(fontSize: 24, color: Colors.white),
+                      ),
+                      Text(
+                        "🚧   Work in progress...   🚧",
+                        style: TextStyle(fontSize: 24, color: Colors.white),
+                      ),
+                    ]),
               ),
-            ]
-          ),
-        ],
+            ),
+            Container(
+              color: Colors.orange,
+              width: 150.0,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    Text(
+                      "Container Laranja",
+                      style: TextStyle(fontSize: 24, color: Colors.blue),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ]),
+        ),
       ),
     );
   }
